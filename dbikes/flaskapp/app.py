d4b51f44ee91af87e561d66101110f52d3f94071
stefanB <stefan-.bordei@ucdconnect.ie>
2021-03-03 18:56:51 +0000

    Flask map demo.

--- dbikes/flaskapp/app.py
+++ dbikes/flaskapp/app.py
@@ -1,10 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template
+from sqlalchemy import create_engine
+import json
+import dbinfo
+import pandas as pd
+
 
 app = Flask(__name__)
 
+DB_NAME = dbinfo.DB_DBIKES_USER
+DB_PASS = dbinfo.DB_DBIKES_PASS
+DB_HOST = dbinfo.DB_DBIKES
+
 @app.route("/")
 def hello():
-    return app.send_static_file("index.html")
+    return render_template("index.html")
+
+
+@app.route("/stations")
+def stations():
+    engine = create_engine(f"mysql+mysqlconnector://{DB_NAME}:{DB_PASS}@{DB_HOST}/dbikes_main", echo=True)
+    df = pd.read_sql_table("static_stations", engine)
+    return df.to_json(orient='records')
+
 
 if __name__ == "__main__":
     app.run(debug=True)

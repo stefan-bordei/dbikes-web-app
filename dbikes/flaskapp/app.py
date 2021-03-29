2ce7fed45eaba7b597a73f3a461112eff564d7d5
Craicden <samuel.hamilton@ucdconnect.ie>
2021-03-29 22:01:09 +0100

    Got the chart functionality working on the Map.html, also basic table display

--- dbikes/flaskapp/app.py
+++ dbikes/flaskapp/app.py
@@ -1,15 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request,session,redirect,url_for
 from sqlalchemy import create_engine
 import json
 import dbinfo
 import pandas as pd
+import datetime
 
 
 app = Flask(__name__)
 
+# cant pass a variable in flask without a super secret key because lord fobid flask makes it easy, anyway this is just a placeholder, though true
+app.secret_key = "Kilkenny is the best county, come at me bro"
+
 DB_NAME = dbinfo.DB_DBIKES_USER
 DB_PASS = dbinfo.DB_DBIKES_PASS
 DB_HOST = dbinfo.DB_DBIKES
+
 
 @app.route("/")
 def hello():
@@ -19,8 +24,14 @@ def hello():
 def plan():
     return render_template("plan.html")
 
-@app.route("/about_us")
+@app.route("/about_us",methods=["GET","POST"])
 def about_us():
+    if request.method == 'POST':
+        data = request.get_json()
+        email = str(data["email"])
+        user_password = str(data["password"])
+        print(user_password, email)
+
     return render_template("about_us.html")
 
 @app.route("/map")
@@ -41,11 +52,37 @@ def stations():
 def prediction():
     return render_template("prediction.html")
 
+
+
+#function that gets the station number from the html webpage and saves it to the session
+@app.route("/varSender",methods=["GET","POST"])
+def varGet():
+    print("IT FUCKIN GOT THIS FAR")
+    if request.method == "POST":
+        data=request.get_json()
+        session["station_number"] = str(data["number"])
+        return redirect(url_for("buttonFunction"))
+
+
+# Return JSON data with stand information from the past week
 @app.route("/btnFunc")
-def buttonPrediction():
+def buttonFunction():
+    number = session.get("station_number",None)
     engine = create_engine(f"mysql+mysqlconnector://{DB_NAME}:{DB_PASS}@{DB_HOST}/dbikes_main", echo=True)
-    df = pd.read_sql_table("dynamic_stations", engine)
+    dateDiff = (datetime.datetime.now() - datetime.timedelta(7)).timestamp()
+    df = pd.read_sql_query(f"SELECT DISTINCT Number, AvailableBikeStands, AvailableBikes, LastUpdate from dynamic_stations WHERE LastUpdate > {dateDiff} AND Number = {number}", engine)
     return df.to_json(orient='records')
+
+
+# Return JSON data with stand information from the past Day
+@app.route("/btnFuncDay")
+def buttonFunctionDay():
+    number = session.get("station_number",None)
+    engine = create_engine(f"mysql+mysqlconnector://{DB_NAME}:{DB_PASS}@{DB_HOST}/dbikes_main", echo=True)
+    dateDiff = (datetime.datetime.now() - datetime.timedelta(1)).timestamp()
+    df = pd.read_sql_query(f"SELECT DISTINCT Number, AvailableBikeStands, AvailableBikes, LastUpdate from dynamic_stations WHERE LastUpdate > {dateDiff} AND Number = {number}", engine)
+    return df.to_json(orient='records')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
